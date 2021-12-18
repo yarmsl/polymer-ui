83f2ecde53393fb67854c2aceea1c1b0dd05d080
Yaroslav Makarov <slideryo@gmail.com>
2021-12-18 20:50:29 +0500

    test22

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ domains=(polidor.ru www.polidor.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="ural.polymer.dev@gmail.com"
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
